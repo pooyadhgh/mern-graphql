118839d067ce1aaa871032bc58afba284d06b1e9
Pooya Dehghani <pooya.dehghani@hm.com>
2022-07-18 10:08:04 +0200

    Add db models

---
+++ server/schema/Project/model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+  clientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' },
+  name: { type: String },
+  description: { type: String },
+  status: { type: String, enum: ['Not Started', 'In Progress', 'Completed'] },
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
