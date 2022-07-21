2968bc44863e4d04a5d289eccecb3cb3c090623d
Pooya Dehghani <pooya.dehghani@hm.com>
2022-07-21 13:57:52 +0200

    Add notfound page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { FaExclamationTriangle } from 'react-icons/fa';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 const NotFound: React.FC = () => {
-  return <div>NotFound</div>;
+  return (
+    <Container className='d-flex my-2 flex-column justify-content-center align-items-center'>
+      <FaExclamationTriangle className='text-danger my-2' size='5em' />
+      <h1 className='my-5'>404 Page Not Found</h1>
+      <LinkContainer to='/'>
+        <Button className='text-white'>Go Back Home</Button>
+      </LinkContainer>
+    </Container>
+  );
 };
 
 export default NotFound;
